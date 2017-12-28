5d292a9491166db7d7dd4ed7e56040eadd4d5517
Mikhail Naganov <mnaganov@google.com>
2017-12-28 13:29:05 -0800

    wayne: Switch to soundtrigger 2.1 implementation
    Bug: 68823037
    Test: "ok google" from locked screen
    Change-Id: I256bae45bc3c68febe5d7bb4bdbd559d752b7d96

--- device.mk
+++ device.mk
@@ -84,8 +84,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.0-service \
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.1-service \
     audio.a2dp.default \
     audio_amplifier.sdm660 \
     audio.primary.sdm660 \
