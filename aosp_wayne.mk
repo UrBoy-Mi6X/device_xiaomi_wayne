c591075468adc3b436e790a0a4e2e31aa2913ac1
MkfsSion <myychina28759@gmail.com>
2018-11-09 13:45:40 +0800

    wayne: PE-ify
    Signed-off-by: MkfsSion <myychina28759@gmail.com>

--- aosp_wayne.mk
+++ aosp_wayne.mk
@@ -19,9 +19,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Lineage stuff
+# Inherit some common PixelExperience stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-$(call inherit-product, vendor/aosp/config/gapps.mk)
 
 # Set boot animation resolution
 TARGET_BOOT_ANIMATION_RES := 1080
